888744aa06652b909bce46f728c9a27f0062970e
zoro <131595713+zero-zoro@users.noreply.github.com>
2023-04-28 15:16:15 +0330

    Create ssh-vpn-UDP.sh
    upload

---
+++ ssh-vpn-UDP.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#coded by: zero-zoro
+if [[ $1 -gt 65535 || $1 -lt 1 ]]; then
+  echo "your port value is out of range"
+  exit 1;
+fi
+
+if [ -z "$1" ]; then
+	udp_port=53
+else
+	udp_port=$1
+fi
+
+apt update -y
+apt install git cmake -y
+git clone https://github.com/ambrop72/badvpn.git /root/badvpn
+mkdir /root/badvpn/badvpn-build
+cd  /root/badvpn/badvpn-build
+cmake .. -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_UDPGW=1 
+wait
+make 
+wait
+cp udpgw/badvpn-udpgw /usr/local/bin/
+cat >> /etc/systemd/system/ssh-udp.service <<EOF
+[Unit]
+Description=SSH-VPN for UDP
+
+[Service]
+User=root
+Restart=always
+Type=simple
+ExecStart=/usr/local/bin/badvpn-udpgw --loglevel none --listen-addr 127.0.0.1:$udp_port
+
+[Install]
+WantedBy=multi-user.target
+EOF
+wait
+systemctl enable ssh-udp.service
+service ssh-udp start
+clear
+echo "your SSH-VPN UDP port: $udp_port \n"
